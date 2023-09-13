d778f7b6c798d5eef52d4ac91f30fe5319fd873d
Avinash S <117369858+Avinashs7@users.noreply.github.com>
2023-09-13 06:37:03 +0530

    Python lab

--- pallindrome.py
+++ pallindrome.py
@@ -1,17 +1,44 @@
-def ispallindrome(num):
-    num1=int(num)
-    rev=0
-    while num1>0:
-        rem=num1%10
-        rev=rev*10+rem
-        num1=num1//10
-    if(rev==int(num)):
-        print("pallindrome")
-def occurence(num):
-    array=[int(x) for x in num]
-    for x in set(array):
-        print(x,"has repeated ",array.count(x),"number of times")
-        array.remove(x)
-num=input()
-ispallindrome(num)
-occurence(num)
+class PaliStr:
+    def __init__(self):
+        self.isPali = False
+
+    def chkPalindrome(self, myStr):
+        if myStr == myStr[::-1]:
+            self.isPali = True
+        else:
+            self.isPali = False
+        return self.isPali
+
+class PaliInt(PaliStr):
+    def __init__(self):
+        self.isPali = False
+
+    def chkPalindrome(self, val):
+        temp = val
+        rev = 0
+        while temp != 0:
+            dig = temp % 10
+            rev = (rev*10) + dig
+            temp = temp //10
+
+        if val == rev:
+            self.isPali = True
+        else:
+            self.isPali = False
+        return self.isPali
+
+st = input("Enter a string : ")
+
+stObj = PaliStr()
+if stObj.chkPalindrome(st):
+    print("Given string is a Palindrome")
+else:
+    print("Given string is not a Palindrome")
+
+val = int(input("Enter a integer : "))    
+
+intObj = PaliInt()
+if intObj.chkPalindrome(val):
+    print("Given integer is a Palindrome")
+else:
+    print("Given integer is not a Palindrome")
